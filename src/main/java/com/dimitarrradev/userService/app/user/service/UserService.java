1f56f100f2ef07318112a22382bc4f5289395fc5
DimitarRadev88 <dimitar.rumenov.radev@gmail.com>
2026-02-13 15:49:23 +0200

    Tests: unit tests for UserService.class

--- src/main/java/com/dimitarrradev/userService/app/user/service/UserService.java
+++ src/main/java/com/dimitarrradev/userService/app/user/service/UserService.java
@@ -7,6 +7,7 @@ import com.dimitarrradev.userService.app.error.exception.UserNotFoundException;
 import com.dimitarrradev.userService.app.error.exception.UsernameAlreadyExistsException;
 import com.dimitarrradev.userService.app.role.enums.RoleType;
 import com.dimitarrradev.userService.app.role.service.RoleService;
+import com.dimitarrradev.userService.app.user.FromModelMapper;
 import com.dimitarrradev.userService.app.user.User;
 import com.dimitarrradev.userService.app.user.UserModel;
 import com.dimitarrradev.userService.app.user.UserModelAssembler;
@@ -16,9 +17,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -26,52 +24,46 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserModelAssembler assembler;
+    private final FromModelMapper fromModelMapper;
     private final RoleService roleService;
 
     @Transactional
     public UserModel createUser(UserAddModel userAddModel) {
-        if (userRepository.existsUserByUsername(userAddModel.username())) {
+        if (this.userRepository.existsUserByUsername(userAddModel.username())) {
             throw new UsernameAlreadyExistsException("Username already exists");
         }
 
-        if (userRepository.existsUserByEmail(userAddModel.email())) {
+        if (this.userRepository.existsUserByEmail(userAddModel.email())) {
             throw new EmailAlreadyExistsException("Email already exists");
         }
 
-        User user = new User(null,
-                userAddModel.username(),
-                userAddModel.firstName(),
-                userAddModel.lastName(),
-                userAddModel.email(),
-                passwordEncoder.encode(userAddModel.password()),
-                userAddModel.weight(),
-                userAddModel.height(),
-                userAddModel.weight() != null && userAddModel.height() != null ? userAddModel.height() / Math.pow(userAddModel.weight(), 2) : null,
-                userAddModel.gym(),
-                new ArrayList<>(List.of(roleService.getRoleByType(RoleType.USER))),
-                null,
-                null
-                );
+        User user = this.fromModelMapper.fromUserAddModel(userAddModel);
 
-        return assembler.toModel(userRepository.save(user));
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(this.roleService.getRoleByType(RoleType.USER));
+        user.setBmi(userAddModel.weight() != null && userAddModel.height() != null ? userAddModel.height() / Math.pow(userAddModel.weight(), 2) : null);
+
+        return this.assembler.toModel(this.userRepository.save(user));
     }
 
     public UserModel getUser(Long id) {
-        User user = userRepository
+        User user = this.userRepository
                 .findById(id)
                 .orElseThrow(() -> new UserNotFoundException("User with that id does not exist!"));
 
-        return assembler.toModel(user);
+        return this.assembler.toModel(user);
     }
 
     public void updateUser(Long id, UserEditModel userEditModel) {
-        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User does not exist"));
+        User user = this.userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User does not exist"));
 
         user.setFirstName(userEditModel.firstName());
         user.setLastName(userEditModel.lastName());
         user.setHeight(userEditModel.height());
         user.setWeight(userEditModel.weight());
-        user.setFirstName(userEditModel.firstName());
+        user.setGym(userEditModel.gym());
 
+        this.userRepository.save(user);
     }
+
 }
---
+++ src/test/java/com/dimitarrradev/userService/app/user/service/UserServiceTests.java
@@ -0,0 +1,275 @@
+package com.dimitarrradev.userService.app.user.service;
+
+import com.dimitarrradev.userService.app.controller.binding.UserAddModel;
+import com.dimitarrradev.userService.app.controller.binding.UserEditModel;
+import com.dimitarrradev.userService.app.error.exception.EmailAlreadyExistsException;
+import com.dimitarrradev.userService.app.error.exception.UserNotFoundException;
+import com.dimitarrradev.userService.app.error.exception.UsernameAlreadyExistsException;
+import com.dimitarrradev.userService.app.role.Role;
+import com.dimitarrradev.userService.app.role.enums.RoleType;
+import com.dimitarrradev.userService.app.role.service.RoleService;
+import com.dimitarrradev.userService.app.user.FromModelMapper;
+import com.dimitarrradev.userService.app.user.User;
+import com.dimitarrradev.userService.app.user.UserModel;
+import com.dimitarrradev.userService.app.user.UserModelAssembler;
+import com.dimitarrradev.userService.app.user.dao.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private UserModelAssembler assembler;
+    @Mock
+    private FromModelMapper fromModelMapper;
+    @Mock
+    private RoleService roleService;
+
+    private UserAddModel userAdd;
+    private UserEditModel userEdit;
+    private User notSavedUser;
+    private User savedUser;
+    private UserModel userModel;
+
+    @BeforeEach
+    void setup() {
+        this.userAdd = new UserAddModel(
+                "username",
+                "first",
+                "last",
+                "some@email.com",
+                "strongpassword",
+                100.0,
+                180.0,
+                "some gym"
+        );
+
+        this.userEdit = new UserEditModel(
+                "new first name",
+                "new last name",
+                101.1,
+                179.9,
+                "new gym"
+        );
+
+        this.notSavedUser = new User(
+                null,
+                this.userAdd.username(),
+                this.userAdd.firstName(),
+                this.userAdd.lastName(),
+                this.userAdd.email(),
+                this.passwordEncoder.encode(userAdd.password()),
+                this.userAdd.weight(),
+                this.userAdd.height(),
+                null,
+                this.userAdd.gym(),
+                new ArrayList<>(),
+                null,
+                null
+        );
+
+        this.savedUser = new User(
+                1L,
+                this.notSavedUser.getUsername(),
+                this.notSavedUser.getFirstName(),
+                this.notSavedUser.getLastName(),
+                this.notSavedUser.getEmail(),
+                this.notSavedUser.getPassword(),
+                this.notSavedUser.getWeight(),
+                this.notSavedUser.getHeight(),
+                this.notSavedUser.getBmi(),
+                this.notSavedUser.getGym(),
+                this.notSavedUser.getRoles(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        this.userModel = new UserModel(
+                this.savedUser.getId(),
+                this.savedUser.getUsername(),
+                this.savedUser.getFirstName(),
+                this.savedUser.getLastName(),
+                this.savedUser.getEmail(),
+                this.savedUser.getWeight(),
+                this.savedUser.getHeight(),
+                this.savedUser.getBmi(),
+                this.savedUser.getGym(),
+                this.savedUser.getCreatedAt(),
+                this.savedUser.getUpdatedAt()
+        );
+    }
+
+    @Test
+    void testCreateUserThrowsWhenUserWithUsernameExists() {
+        when(this.userRepository.existsUserByUsername(this.userAdd.username()))
+                .thenReturn(true);
+
+        assertThrows(UsernameAlreadyExistsException.class,
+                () -> this.userService.createUser(this.userAdd));
+    }
+
+    @Test
+    void testCreateUserThrowsWhenUserWithEmailExists() {
+        when(this.userRepository.existsUserByUsername(this.userAdd.username()))
+                .thenReturn(false);
+
+        when(this.userRepository.existsUserByEmail(this.userAdd.email()))
+                .thenReturn(true);
+
+        assertThrows(EmailAlreadyExistsException.class,
+                () -> this.userService.createUser(this.userAdd));
+    }
+
+    @Test
+    void testCreateUserSavesNewUserInRepository() {
+        when(this.userRepository.existsUserByUsername(this.userAdd.username()))
+                .thenReturn(false);
+
+        when(this.userRepository.existsUserByEmail(this.userAdd.email()))
+                .thenReturn(false);
+
+
+        when(this.fromModelMapper.fromUserAddModel(this.userAdd))
+                .thenReturn(this.notSavedUser);
+
+        when(this.passwordEncoder.encode(this.notSavedUser.getPassword()))
+                .thenReturn("encoded password");
+
+        when(this.roleService.getRoleByType(RoleType.USER))
+                .thenReturn(new Role(1L, RoleType.USER, new ArrayList<>()));
+
+        this.userService.createUser(this.userAdd);
+
+        assertEquals(this.notSavedUser.getHeight() / Math.pow(this.notSavedUser.getWeight(), 2), this.notSavedUser.getBmi());
+
+        verify(this.userRepository, times(1)).save(this.notSavedUser);
+
+
+    }
+
+    @Test
+    void testCreateUserReturnsUserModelRepository() {
+        when(this.userRepository.existsUserByUsername(this.userAdd.username()))
+                .thenReturn(false);
+
+        when(this.userRepository.existsUserByEmail(this.userAdd.email()))
+                .thenReturn(false);
+
+        when(this.fromModelMapper.fromUserAddModel(this.userAdd))
+                .thenReturn(this.notSavedUser);
+
+        when(this.passwordEncoder.encode(this.notSavedUser.getPassword()))
+                .thenReturn("encoded password");
+
+        when(this.roleService.getRoleByType(RoleType.USER))
+                .thenReturn(new Role(1L, RoleType.USER, new ArrayList<>()));
+
+        when(this.userRepository.save(this.notSavedUser))
+                .thenReturn(this.savedUser);
+
+        UserModel expected = new UserModel(
+                this.savedUser.getId(),
+                this.savedUser.getUsername(),
+                this.savedUser.getFirstName(),
+                this.savedUser.getLastName(),
+                this.savedUser.getEmail(),
+                this.savedUser.getWeight(),
+                this.savedUser.getHeight(),
+                this.savedUser.getBmi(),
+                this.savedUser.getGym(),
+                this.savedUser.getCreatedAt(),
+                this.savedUser.getUpdatedAt()
+        );
+
+        when(this.assembler.toModel(this.savedUser))
+                .thenReturn(expected);
+
+        assertEquals(expected, this.userService.createUser(this.userAdd));
+    }
+
+    @Test
+    void testGetUserThrowsWhenUserDoesNotExist() {
+        when(this.userRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class,
+                () -> this.userService.getUser(1L)
+        );
+    }
+
+    @Test
+    void testGetUserReturnsCorrectUserModel() {
+        User user = new User(
+                1L,
+                this.userAdd.username(),
+                this.userAdd.firstName(),
+                this.userAdd.lastName(),
+                this.userAdd.email(),
+                this.passwordEncoder.encode(userAdd.password()),
+                this.userAdd.weight(),
+                this.userAdd.height(),
+                this.userAdd.height() / Math.pow(this.userAdd.weight(), 2),
+                this.userAdd.gym(),
+                new ArrayList<>(List.of(new Role(1L, RoleType.USER, Collections.emptyList()))),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        when(this.userRepository.findById(1L))
+                .thenReturn(Optional.of(user));
+
+
+        when(this.assembler.toModel(user))
+                .thenReturn(this.userModel);
+
+        assertEquals(this.userModel, this.userService.getUser(user.getId()));
+    }
+
+    @Test
+    void testUpdateUserThrowsWhenUserDoesNotExist() {
+        when(this.userRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class,
+                () -> this.userService.updateUser(1L, new UserEditModel(null, null, null, null, null))
+        );
+    }
+
+    @Test
+    void testUpdateUserSavesUserWithNewData() {
+        when(this.userRepository.findById(this.savedUser.getId()))
+                .thenReturn(Optional.of(savedUser));
+
+        this.userService.updateUser(this.savedUser.getId(), this.userEdit);
+
+        assertEquals(this.userEdit.firstName(), this.savedUser.getFirstName());
+        assertEquals(this.userEdit.lastName(), this.savedUser.getLastName());
+        assertEquals(this.userEdit.height(), this.savedUser.getHeight());
+        assertEquals(this.userEdit.weight(), this.savedUser.getWeight());
+        assertEquals(this.userEdit.gym(), this.savedUser.getGym());
+
+        verify(this.userRepository, times(1)).save(this.savedUser);
+    }
+
+}
